96875989506cd38036d565d51340839376bfee38
Mhirley Lopes <mhiloca@gmail.com>
2019-08-26 16:53:23 -0300

    changed function name

--- walk.py
+++ walk.py
@@ -1,4 +1,4 @@
-def isValidWalk(walk):
+def is_valid_walk(walk):
     if len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w'):
         return True
     return False
