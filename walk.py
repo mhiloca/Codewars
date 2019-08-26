1048d89423d1b18610823c22a223752f90d99513
Mhirley Lopes <mhiloca@gmail.com>
2019-08-26 16:36:09 -0300

    add feature-1

--- walk.py
+++ walk.py
@@ -1,4 +1,7 @@
-def isValidWalk(walk):
+# Program for a 10min walk with directions to go and come back
+
+
+def is_valid_walk(walk):
     if len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w'):
         return True
     return False
