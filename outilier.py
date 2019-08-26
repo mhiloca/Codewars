6fdcde5a0aa5c6df86278c5a1e1bc05e6966c36a
Mhirley Lopes <mhiloca@gmail.com>
2019-08-26 19:17:56 -0300

    added func description

--- outilier.py
+++ outilier.py
@@ -1,3 +1,11 @@
+"""
+You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+The array is either entirely comprised of odd integers or entirely comprised of even integers
+except for a single integer N.
+Write a method that takes the array as an argument and returns this "outlier" N.
+"""
+
+
 def find_outlier(integers):
     evens = [x for x in integers if x % 2 == 0]
     odds = [y for y in integers if y % 2 != 0]
--- prime_number.py
+++ prime_number.py
@@ -1,3 +1,10 @@
+"""
+Define a function that takes an integer argument and returns logical value true or false
+depending on if the integer is a prime.
+
+"""
+
+
 def is_prime(num):
     if num < 2:
         return False
