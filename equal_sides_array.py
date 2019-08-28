0c7fe46c93a7e0cd3d5d0e81d0082f801229781a
Mhirley Lopes <mhiloca@gmail.com>
2019-08-28 16:46:45 -0300

    updated equal_sides

--- equal_sides_array.py
+++ equal_sides_array.py
@@ -56,13 +56,13 @@ def find_even_index_v2(arr):
     return -1
 
 
-print(find_even_index([1,2,3,4,3,2,1]))
-print(find_even_index([1,100,50,-51,1,1]))
-print((find_even_index([1,2,3,4,5,6])))
-print((find_even_index([20,10,30,10,10,15,35])))
-print((find_even_index([20,10,-80,10,10,15,35])))
-print((find_even_index([10,-80,10,10,15,35,20])))
-print((find_even_index(range(1,100))))
-print((find_even_index([0,0,0,0,0])))
-print((find_even_index([-1,-2,-3,-4,-3,-2,-1])))
-print((find_even_index(range(-100,-1))))
+print(find_even_index_v2([1,2,3,4,3,2,1]))
+print(find_even_index_v2([1,100,50,-51,1,1]))
+print((find_even_index_v2([1,2,3,4,5,6])))
+print((find_even_index_v2([20,10,30,10,10,15,35])))
+print((find_even_index_v2([20,10,-80,10,10,15,35])))
+print((find_even_index_v2([10,-80,10,10,15,35,20])))
+print((find_even_index_v2(range(1,100))))
+print((find_even_index_v2([0,0,0,0,0])))
+print((find_even_index_v2([-1,-2,-3,-4,-3,-2,-1])))
+print((find_even_index_v2(range(-100,-1))))
